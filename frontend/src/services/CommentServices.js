5546b4d7fa04a754376a8485db49e3e9241d8d03
Ahmed Semih Erkan <ahmedsemiherkan@gmail.com>
2022-07-27 15:39:41 +0300

    Comment services created

---
+++ frontend/src/services/CommentServices.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+
+export const getAllComments = async () => {
+    const { data } = axios.get(`${process.env.REACT_APP_API_BASE_URL}/comments`);
+    return data;
+};
+
+export const getCommentById = async (id) => {
+    const { data } = axios.get(`${process.env.REACT_APP_API_BASE_URL}/comments/${id}`);
+    return data;
+};
+
+export const getCommentByAuthorId = async (id) => {
+    const { data } = axios.get(`${process.env.REACT_APP_API_BASE_URL}/comments/author/${id}`);
+    return data;
+};
+
+export const getCommentByProductId = async (id) => {
+    const { data } = axios.get(`${process.env.REACT_APP_API_BASE_URL}/comments/product/${id}`);
+    return data;
+};
+
+export const addComment = async (productId, comment, author) => {
+    const { data } = axios.post(`${process.env.REACT_APP_API_BASE_URL}/comments`, {
+        for: productId,
+        comment,
+        author
+    });
+    return data;
+};
+
+export const updateComment = async (id, productId, comment, author) => {
+    const { data } = axios.put(`${process.env.REACT_APP_API_BASE_URL}/comments/${id}`, {
+        for: productId,
+        comment,
+        author
+    });
+    return data;
+};
+
+export const deleteComment = async (id) => {
+    const { data } = axios.delete(`${process.env.REACT_APP_API_BASE_URL}/comment/${id}`);
+    return data;
+};
